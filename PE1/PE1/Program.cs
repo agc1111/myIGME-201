16d4aaaad8925c61c435bb1eba6ac943886d464d
Aidan Carvajal <agc1111@rit.edu>
2022-08-26 17:05:34 -0400

    SquashBugs

--- PE1/PE1/Program.cs
+++ PE1/PE1/Program.cs
@@ -7,9 +7,26 @@ using System.Threading.Tasks;
 namespace PE1
 {
     internal class Program
+    /*
+     * Aidan Carvajal
+     * To test and write a line and experiment with varibales and math
+    */
     {
         static void Main(string[] args)
+        /*
+         * Used to print a line and variable 
+        */
         {
+            Console.WriteLine("Dedede's Royal Payback");
+
+            int varibaleNum = 0;
+
+            for(int i = 0; i < 5; i++)
+            {
+                varibaleNum++;
+            }
+
+            Console.WriteLine(varibaleNum);
         }
     }
 }
---
+++ SquashTheBugs/SquashTheBugs/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquashTheBugs
+{
+    // Class Program
+    // Author: Aidan Carvajal
+    // Purpose: Bug squashing exercise
+    // Restrictions: None
+    class Program
+    {
+        // Method: Main
+        // Purpose: Loop through the numbers 1 through 10 
+        // Output N/(N-1) for all 10 numbers
+        // and list all numbers processed
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            // declare int counter
+            //int i = 0
+            double i = 0; //no semicolin; change to double
+            string allNumbers = null;
+
+            // loop through the numbers 1 through 10
+            //for (i = 1.0; i < 10; ++i)
+            for (i = 1.0; i <= 10; ++i) //Must be <= 10 to include 10
+            {
+                // declare string to hold all numbers
+                //string allNumbers = null; allNumbers declared outside of loop
+
+                // output explanation of calculation
+                //Console.Write(i + "/" + i - 1 + " = ");
+                Console.Write(i + "/" + (i - 1) + " = "); //No paranthises
+
+                // output the calculation based on the numbers
+                Console.WriteLine(i / (i - 1));
+
+                // concatenate each number to allNumbers
+                allNumbers += i + " ";
+
+                // increment the counter
+                //i += 1; Get rid of counter to process all numbers
+            }
+
+            // output all numbers which have been processed
+            //Console.WriteLine("These numbers have been processed: " allNumbers);
+            Console.WriteLine("These numbers have been processed: " + allNumbers);
+        }
+    }
+}
+
