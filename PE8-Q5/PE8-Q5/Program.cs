35da4c88e886355f2b1e0326d87132adc0d17c7e
Aidan Carvajal <agc1111@rit.edu>
2022-09-14 23:37:45 -0400

    PE8-Q5

---
+++ PE8-Q5/PE8-Q5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_Q5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[,,] arr = new double[,,] { {X, y, z};
+
+            for (int i = 0;)
+            {
+                
+            }
+            
+
+        }
+    }
+}
---
+++ TeamProjectPitch-Aifan/TeamProjectPitch-Aifan/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProjectPitch_Aifan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
