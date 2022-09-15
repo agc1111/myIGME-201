1d6249b2d21a542b439ce3dc7aecc784ca1b4d55
Aidan Carvajal <agc1111@rit.edu>
2022-09-14 23:44:49 -0400

    PE8-Q7

---
+++ PE8-Q7/PE8-Q7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_Q7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a string you want reversed: ");
+            string userLine = Console.ReadLine();
+            string reverse = "";
+
+            for(int i = userLine.Length - 1; i >= 0; i--)
+            {
+                reverse += userLine[i];
+            }
+
+            Console.Write("Your string reversed: ");
+            Console.WriteLine(reverse);
+        }
+    }
+}
