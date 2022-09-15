fdc4cc870b57f9c5731c19f59eec851f4219ab81
Aidan Carvajal <agc1111@rit.edu>
2022-09-15 00:00:08 -0400

    PE7-Q8

---
+++ PE7_Q8/PE7_Q8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE7_Q8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a string with 'yes' or 'no' in it:");
+            string userLine = Console.ReadLine();
+            string resultLine = userLine.ToLower();
+
+            resultLine.Replace('no', 'yes').Replace('yes', 'no');
+
+            Console.WriteLine("Your new string: ");
+            Console.WriteLine(resultLine);
+        }
+    }
+}
