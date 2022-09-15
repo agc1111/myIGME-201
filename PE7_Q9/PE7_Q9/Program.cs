89cb8e433cc2a6c31e2f20ace0306198ba2fa4ee
Aidan Carvajal <agc1111@rit.edu>
2022-09-15 00:10:40 -0400

    PE7_Q9

---
+++ PE7_Q9/PE7_Q9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE7_Q9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a string:");
+            string line =  Console.ReadLine();
+            string dq;
+
+            for(int i = 0; i < line.Length; i++)
+            {
+                dq += line[i];
+
+                dq.Replace(" ", "'");
+            }
+
+        }
+    }
+}
