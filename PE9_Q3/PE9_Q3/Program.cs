2e535dec5820511e2a0a63ea06307fb23bcec134
Aidan Carvajal <agc1111@rit.edu>
2022-09-17 23:56:19 -0400

    PE9_Q3

---
+++ PE9_Q3/PE9_Q3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE9_Q3
+{
+    internal class Program
+    {
+        public delegate void del(string a);
+        static void Main(string[] args)
+        {
+
+            
+
+            
+
+        }
+
+        public static void impseronateMessage(string impersonate)
+        {
+            Console.WriteLine("")
+        }
+
+        
+
+        
+    }
+}
