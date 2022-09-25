89f5c77f4ae816b94385fa4e8313d0f7be2490ef
Aidan Carvajal <agc1111@rit.edu>
2022-09-25 18:46:34 -0400

    UT1_Q12

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /*
+     * Aidan Carvajal
+     * Unit Test 1, Question 12
+     */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sName;
+            double dSalary = 30000;
+
+            //Prompt for a name
+            Console.WriteLine("Enter your name: ");
+            sName = Console.ReadLine();
+
+            bool raise = GiveRaise(sName, ref dSalary);
+
+            if(raise == true)
+            {
+                Console.WriteLine("Congragulations! You got a raise! Your new salary is: " + dSalary);
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you did not get a raise.");
+            }
+
+        }
+        static bool GiveRaise(string name, ref double salary)
+        {
+            bool tf = false;
+            string myName = "Aidan";
+
+            if(name == myName)
+            {
+                salary += 19999.999;
+                tf = true;
+            }
+
+            return tf;
+
+        }
+
+    }
+}
